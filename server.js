1a3bad56227b5c7ff41b06336c23aa4321e32cee
Kuba Wlodarczyk <j.wlodarczyk@sunscrapers.com>
2018-06-16 16:12:22 +0200

    cors and OPTIONS request method

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var Web3 = require('web3');
+// var Web3 = require('web3');
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8000;
 var app = express();
 
 app.use(bodyParser.json()); // support json encoded bodies
@@ -12,23 +12,42 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 // const web3 = new Web3( new Web3.providers.HttpProvider(testnet) );
 
 app.post('/kyc', function(req, res) {
-  console.log("Received data");
-  var token = req.body.token;
-  var wallet = req.body.wallet;
-  var hashedData = req.body.hashed_data;
 
-  // TODO: call alior with the token to get user details
-  var result = "hey";
-  //TODO: var hashFromBank = Web3.sha3(result);
-  var hashFromBank = "something";
-  if (hashFromBank === hashedData) {
-    //TODO: call method on Smart Contract
+
+
+  if (req.method === 'OPTIONS') {
+    console.log('!OPTIONS');
+    var headers = {};
+    // IE8 does not allow domains to be specified, just the *
+    // headers["Access-Control-Allow-Origin"] = req.headers.origin;
+    headers["Access-Control-Allow-Origin"] = "*";
+    headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+    headers["Access-Control-Allow-Credentials"] = false;
+    headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+    headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+    res.writeHead(200, headers);
+    res.end();
+  } else {
+
+
+    console.log("Received data");
+    var token = req.body.token;
+    var wallet = req.body.wallet;
+    var hashedData = req.body.hashed_data;
+
+    // TODO: call alior with the token to get user details
+    var result = "hey";
+    //TODO: var hashFromBank = Web3.sha3(result);
+    var hashFromBank = "something";
+    if (hashFromBank === hashedData) {
+      //TODO: call method on Smart Contract
+    }
+    res.send(wallet + ' ' + token + ' ' + hashedData);
   }
-  res.send(wallet + ' ' + token + ' ' + hashedData);
 });
 
 app.get('/', function(req, res) {
-  console.log(web3);
+  // console.log(web3);
   res.send('Hello from BlocKey!');
 });
 
@@ -37,9 +56,41 @@ app.get('/success', function (req, res) {
   res.send('Success');
 })
 
+
 app.use(function(req, res, next) {
-  res.status(404).send
+    var oneof = false;
+    if (req.headers.origin) { //req.headers.origin.match(/whateverDomainYouWantToWhitelist/g) ) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        oneof = true;
+    }
+    if (req.headers['access-control-request-method']) {
+        res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+        oneof = true;
+    }
+    if (req.headers['access-control-request-headers']) {
+        res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+        oneof = true;
+    }
+    if (oneof) {
+        res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+    }
+
+    // intercept OPTIONS method
+    if (oneof && req.method == 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
 });
+
+
+
+
+
+
+
+
+
 
 app.listen(port, function() {
   console.log('Our app is running on http://localhost:' + port);
