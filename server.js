e01d60fad5bdbf55f29cebb0c4edacd43dd18758
pbrudny <pbrudny@gmail.com>
2018-06-16 14:24:48 +0200

    Add port for heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var Web3 = require('web3');
 
+var port = process.env.PORT || 8080;
 var app = express();
 
 app.use(bodyParser.json()); // support json encoded bodies
@@ -40,6 +41,6 @@ app.use(function(req, res, next) {
   res.status(404).send
 });
 
-var server = app.listen(3000, function() {
-  console.log('Listening on http://localhost:3000');
+app.listen(port, function() {
+  console.log('Our app is running on http://localhost:' + port);
 });
